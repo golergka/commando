e3bd6baf4970a5a52378176eea3a3befe04c037d
Max Yankov <golergka@gmail.com>
2014-08-19 23:36:27 +0400

    Basic parallax movement

---
+++ Assets/ParallaxController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ParallaxController : MonoBehaviour
+{
+
+	[System.Serializable]
+	public class ParallaxLayer
+	{
+		public Transform	Root;
+		public float		Multiplier;
+	}
+
+	public List<ParallaxLayer> Layers;
+
+	public void MoveBy(Vector3 _Delta)
+	{
+		foreach(var l in Layers)
+		{
+			l.Root.position += _Delta * l.Multiplier;
+		}
+	}
+}
---
+++ Assets/ParallaxInputTester.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxInputTester : MonoBehaviour
+{
+	ParallaxController m_ParallaxController;
+
+	void Start()
+	{
+		m_ParallaxController = gameObject.GetComponent<ParallaxController>();
+		if (m_ParallaxController == null)
+		{
+			Debug.LogError("Can't find parallax controller!");
+			enabled = false;
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float movement = Input.GetAxis("Horizontal");
+		m_ParallaxController.MoveBy(new Vector3(movement, 0, 0));
+	}
+}
