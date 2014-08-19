24eb8678db14838bdc6c50cb90cc4c364f9ffdfc
Max Yankov <golergka@gmail.com>
2014-08-19 21:58:52 +0400

    Pixel perfect camera

---
+++ Assets/PixelPerfectCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelPerfectCamera : MonoBehaviour
+{
+	int m_ScreenHeight;
+
+	int ScreenHeight
+	{
+		get { return m_ScreenHeight; }
+		set
+		{
+			if (m_ScreenHeight == value)
+				return;
+
+			camera.orthographicSize = ((float) value) / 2;
+			Debug.Log("Set camera size to " + camera.orthographicSize);
+			m_ScreenHeight = value;
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		ScreenHeight = Screen.height;
+	}
+}
