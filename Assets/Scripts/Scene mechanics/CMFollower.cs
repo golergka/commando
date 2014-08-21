18036a9051e240239905fcf43ebf564b9c85ad9f
Max Yankov <golergka@gmail.com>
2014-08-21 22:03:56 +0400

    Fixed follower script

--- Assets/Scripts/Scene mechanics/CMFollower.cs
+++ Assets/Scripts/Scene mechanics/CMFollower.cs
@@ -5,6 +5,10 @@ public class CMFollower : CMBehavior
 {
 	public Transform Followee { get; set; }
 
+	public bool FollowX;
+	public bool FollowY;
+	public bool FollowZ;
+
 	public float FollowSpeed = 1f;
 	public float FollowSnap = 0.02f;
 
@@ -14,14 +18,15 @@ public class CMFollower : CMBehavior
 			return;
 
 		var target = new Vector3(
-				Followee.position.x,
-				Followee.position.y,
-				transform.position.z
+				FollowX ? Followee.position.x : transform.position.x,
+				FollowY ? Followee.position.y : transform.position.y,
+				FollowZ ? Followee.position.z : transform.position.z
 			);
 		var delta = target - transform.position;
 		if (delta.magnitude < FollowSnap)
 		{
 			transform.position = target;
+
 		}
 		else
 		{
