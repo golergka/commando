321a8d39bb4f5674ca8480034c65f6212e14a797
Max Yankov <golergka@gmail.com>
2014-08-21 22:27:11 +0400

    Animated character movement

--- Assets/Scripts/Actors/CMCommandoActor.cs
+++ Assets/Scripts/Actors/CMCommandoActor.cs
@@ -3,15 +3,54 @@ using UnityEngine;
 public class CMCommandoActor : CMBehavior
 {
 	public float Speed = 1f;
+	public float SpriteSpeed = 5f;
+
+	public Sprite[] Sprites;
+
+	int		m_CurrentSprite;
+	int		CurrentSprite
+	{
+		get { return m_CurrentSprite; }
+		set
+		{
+			m_CurrentSprite = value % Sprites.Length;
+			if (m_CurrentSprite < 0)
+				m_CurrentSprite += Sprites.Length;
+			SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+			renderer.sprite = Sprites[m_CurrentSprite];
+		}
+	}
+
+	float	m_SpriteProgress;
+	float	SpriteProgress
+	{
+		get { return m_SpriteProgress; }
+		set
+		{
+			m_SpriteProgress = value;
+			while(m_SpriteProgress > 1f)
+			{
+				m_SpriteProgress--;
+				CurrentSprite++;
+			}
+			while(m_SpriteProgress < 0f)
+			{
+				m_SpriteProgress++;
+				CurrentSprite--;
+			}
+		}
+	}
 
 	void Start()
 	{
 		CameraController.gameObject.GetOrAddComponent<CMFollower>().Followee = transform;
+		CurrentSprite = 0;
 	}
 
 	void Update()
 	{
-		float movement = Input.GetAxis("Horizontal");
-		transform.position += new Vector3(movement, 0, 0) * Speed * Time.deltaTime;
+		float movement = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+		transform.position += new Vector3(movement, 0, 0);
+		SpriteProgress += movement * SpriteSpeed;
 	}
 }
