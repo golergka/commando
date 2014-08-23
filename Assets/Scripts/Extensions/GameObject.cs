6a133c9921b4d765885dc451c1e4a232c2c8fa39
Max Yankov <golergka@gmail.com>
2014-08-23 15:48:50 +0400

    Fixed GameObject.GetOrAddComponent<T>()

--- Assets/Scripts/Extensions/GameObject.cs
+++ Assets/Scripts/Extensions/GameObject.cs
@@ -4,6 +4,20 @@ public static class GameObjectExtensions
 {
 	public static T GetOrAddComponent<T>(this GameObject _GameObject) where T : Component
 	{
-		return _GameObject.GetComponent<T>() ?? _GameObject.AddComponent<T>();
+		if (_GameObject == null)
+		{
+			Debug.LogError("_GameObject is null!");
+			return null;
+		}
+		var result = _GameObject.GetComponent<T>();
+		if (result == null)
+		{
+			result = _GameObject.AddComponent<T>();
+		}
+		if (result == null)
+		{
+			Debug.LogError("Couldn't add the component!");
+		}
+		return result;
 	}
 }
